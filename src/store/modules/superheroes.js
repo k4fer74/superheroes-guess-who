3cdd6b4b15e2a2d7ea673592940f1dd5648194fe
Matheus Lugon <matheus@l4u.com.br>
2018-04-20 19:29:47 -0300

    add toggle hero method

--- src/store/modules/superheroes.js
+++ src/store/modules/superheroes.js
@@ -1,23 +1,40 @@
 import superheroes from '../../api/superheroes'
 
 const state = {
-  all: []
+  all: [],
+  hidden: [],
+  selected: 0
 }
 
 const getters = {
-  allHeroes: state => state.all
+  allHeroes: state => state.all,
+  isHidden: (state) => (hero) => {
+    return state.hidden.indexOf(hero.id) >= 0
+  }
 }
 
 const actions = {
   getAllHeroes ({commit}) {
     const heroes = superheroes.getAllHeroes()
     commit('setHeroes', heroes)
+  },
+  toggleHero ({commit}, hero) {
+    commit('toggleHero', hero)
   }
 }
 
 const mutations = {
   setHeroes(state, heroes) {
     state.all = heroes
+  },
+  toggleHero (state, hero) {
+    const index = state.hidden.indexOf(hero.id)
+    if (index < 0) {
+      state.hidden.push(hero.id)
+    }
+    else {
+      state.hidden.splice(index, 1)
+    }
   }
 }
 
